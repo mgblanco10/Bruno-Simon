8b91d0e3cc8845c49e391f2386ab021a48eb4dc6
Mónica Blanco Calviño <mgblanco1001@gmail.com>
2024-01-12 12:14:56 +0100

    wip

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,34 +1,32 @@
 // console.log(THREE)
-
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 //Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 'red'})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 //Sizes
-const sizes ={
-    width:800,
-    height:600
-}
+const sizes = {
+  width: 800,
+  height: 600,
+};
 
 // //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3; 
-// camera.position.x = 3; 
+camera.position.z = 3;
+// camera.position.x = 3;
 // camera.position.y = 1;
-scene.add(camera)
+scene.add(camera);
 
 // Renderer
-const canvas = document.querySelector('.webgl');
+const canvas = document.querySelector(".webgl");
 console.log(canvas);
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
 });
 
-renderer.setSize(sizes.width, sizes.height)
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-
